8c4cc90b4540cb44600797480be8bf9bb861ab01
riyasingh08 <96309453+riyasingh08@users.noreply.github.com>
2022-09-21 22:20:41 +0530

    Add files via upload

---
+++ Backend/flight-search/src/main/java/com/capg/flightsearch/Repository/FlightRepository.java
@@ -0,0 +1,24 @@
+package com.capg.flightsearch.Repository;
+
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.capg.flightsearch.Entity.Flight;
+
+@Repository
+public interface FlightRepository extends MongoRepository<Flight, String> {
+
+	//List<Flight> findFlightInBetween(String origin, String destination);
+
+	@Query("{origin: ?0, destination: ?1}")
+	List<Flight> findFlightInBetween(String origin, String destination);
+////	List<Flight> findByOriginAndDestinationAndFlightDate(String origin,String destination, String flightDate);
+////
+////	Flight findByFlightNumberAndFlightDate(String flightNumber, String flightDate);
+
+}
